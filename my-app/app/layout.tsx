ac6e1e0f42857b0c381e680d2ae47bf5129fbd17
gs1pakistan <gs1pk.org@gmail.com>
2026-01-13 07:36:29 +0000

    Your commit message here

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,14 @@
+// app/layout.tsx
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased" suppressHydrationWarning>
+        {children}
+      </body>
+    </html>
+  )
+}
